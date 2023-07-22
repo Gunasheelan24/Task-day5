b65d66e1a6e5b56bd8e1c10834d0a55b295a3b1f
Gunasheelan24 <140253087+Gunasheelan24@users.noreply.github.com>
2023-07-22 18:54:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,98 @@
+// 1.For the given json iterate over all for loops 
+
+// let Person = { 
+//     'name':'gunasheelan',
+//     'age':23,
+//     'location':'Coimbatore'
+// }
+// let finalResult = Object.keys(Person);
+
+// //1. for loop
+// for(let d = 0; d < finalResult.length;d++)  {
+//     console.log(Person[finalResult[d]]);
+// }
+
+//2.for in loop 
+// for (const Data in Person) { 
+//     console.log(Person[Data]);
+// }
+
+//3.for of 
+// for(let Data of finalResult) { 
+//     console.log(Person[Data]);
+// }
+
+//4.foreach
+//finalResult.forEach(values);
+//function values(item) {
+//console.log(Person[item]);
+//}
+
+
+// 2.Create Resume using Json 
+// const Resume = { 
+//      'My Details':{
+//      'name':'Gunasheelan A',
+//     ' Date of birth':'24.05.2000',
+//      'Gender':'Male',
+//      'Phone':9342575974,
+//      'Nationality':'Indian',
+//      'Marital_Status':'Single',
+//      'E-mail':'gunasheelan1624@gmail.com',
+//      'Father Name':'Arullchelvan SD',
+//      'Mother Name':'Sivaa Arullchelvan',
+//     },
+//      'location':{
+//         'country':"India",
+//         'State':'Tamil Nadu',
+//         'District':'Coimbatore',
+//         'Location':'4/44 First Lane Lakshmi Nagar Kinathukadavoo Coimbatore',
+//         'Pincode':642109,
+//      },
+//      'Education':{
+//         'Kg To 5th':'Prabhakar Memorial Nursery And Primary School',
+//         '5th to 10th':'New barath matriculation school',
+//         '10th To 12th':' The Merit Matriculation Higher Secondary School',
+//         'Collage':'Hindusthan Institute Of Technology',
+//         'Degree':'B.TECH',
+//         'Branch':'Information Technology',
+//      },
+//      'Collage Project' : {
+//         'Name':"Real Time Drowsiness Detection Using Open Cv",
+//      },
+//      'Languages':{
+//         'Mother toung':'Tamil',
+//         'Second Language':'English'
+//      },
+//      'My Hobby' : {
+//         'Names':"Playing games in outdoor, Reading and writing articles, Watching Series in ott platforms",
+//      },
+//      'My Skills': { 
+//         'name':'Html,css,python,Core java'
+//      }
+// }
+// console.log(Resume);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
